c616a3205ee49f90af98e1d876f3439d28a9ee65
Dima Konstantinov <dkonst8@gmail.com>
2020-02-22 12:34:10 +0800

    Первый коммит

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+int i = 1; 
+
+void setup()
+{
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println(i++);
+  delay(1); // delay in between reads for stability
+}
